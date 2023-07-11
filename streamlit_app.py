aa639b18eea333202e7ae7ca2e54ac8db3842ba8
DIR\stephen.kurniawan <stepkurniawan@gmail.com>
2023-07-11 11:47:07 +0200

    add menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,23 @@
 import streamlit as st
 
 st.title('My Parents New Healthy Diner')
+
+st.write('Welcome to my parents new healthy diner. We are excited to have you here. Please select your order from the menu below.')
+
+menu = ['Salad', 'Soup', 'Sandwich', 'Burger', 'Pizza']
+option = st.selectbox('Menu', menu)
+
+st.write('You selected:', option)
+
+if option == 'Salad':
+    st.write('You selected the salad. This is a great choice. It is very healthy and low in calories.')
+elif option == 'Soup':
+    st.write('You selected the soup. This is a great choice. It is very healthy and low in calories.')
+elif option == 'Sandwich':
+    st.write('You selected the sandwich. This is a great choice. It is very healthy and low in calories.')
+elif option == 'Burger':
+    st.write('You selected the burger. This is not a great choice. It is very unhealthy and high in calories.')
+elif option == 'Pizza':
+    st.write('You selected the pizza. This is not a great choice. It is very unhealthy and high in calories.')
+
+st.write('Thank you for visiting our diner. We hope you enjoyed your meal. Please come again soon.')
